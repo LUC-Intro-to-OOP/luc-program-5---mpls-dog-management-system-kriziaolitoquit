0339c6f50f6542a563b6bdda69c440e8096e7840
Krizia <kolitoquit@luc.edu>
2022-09-24 20:00:13 -0500

    assignment5

--- src/DogManagement.java
+++ src/DogManagement.java
@@ -26,8 +26,8 @@ Program only accepts dog id numbers that have been entered.
 
 ## Requirement 3 (5 Points) 
 The program should consist of a minimum of 5 methods.  
-    1) A welcome method that describes the program.  
-    2) A method to display the selection prompt
+    1) A welcome method that describes the program.  OK
+    2) A method to display the selection prompt OK
     3) A method that allows the care attendant to enter a record.  
     4) A method that allows the care attendant to update a record and 
     5) A method that allows the care attendant to retrieve an existent record.
@@ -42,15 +42,31 @@ Program is submitted by the due date listed and pushed to assigned GitHub Reposi
     [REPLACE MY INFORMATION WITH YOURS]
     Course: COMP 170, Fall 1 2022
     System: Visual Studio Code, Windows 10
-    Author: C. Fulton
+    Author: K. Olitoquit
 */
 
+
+//keep track of the index of each dog
+//keep a counter for each additional dog
+//increment a count when 1 is selected
+
+import java.util.ArrayList;
 import java.util.Scanner; //Importing Scanner Class
 public class DogManagement {
     /*
      * Global Declaration for parallel arrays and Scanner Object
      */
-    //DECLARING PARALEL ARRAYS OUTSIDE OF MAIN METHOD TO HOLD DOG DATA use the static keyword
+    //DECLARING PARALLEL ARRAYS OUTSIDE OF MAIN METHOD TO HOLD DOG DATA use the static keyword
+
+    static int [] dogsAge = new int[12];
+    static ArrayList<Integer> dogsAgeList = new ArrayList<>();
+    static String [] dogsName = new String[12];
+    static ArrayList<String> dogsNameList = new ArrayList<>();
+    static int [] dogsID = new int[12];
+    static ArrayList<Integer> dogsIDList = new ArrayList<>();
+    static int [] dogsWeight = new int[12];
+    static ArrayList<Integer> dogsWeightList = new ArrayList<>();
+    static int i = 0; //counter
 
 
     //DECLARING SCANNER OBJECT
@@ -58,7 +74,38 @@ public class DogManagement {
 
     public static void main(String[] args) throws Exception {
         
+        int prompt = ' ';
         
+        welcome();
+        
+        
+        //Looping structure is in main class
+
+        do {
+
+            //variable
+            prompt = displayPrompt();
+
+            if(prompt < 1 || prompt > 4){
+                System.out.println("Invalid menu option");
+            } else if (prompt == 1) 
+            {
+                
+                createupdateDogRecord(-1);
+                i++;
+            } else if (prompt == 2 || prompt == 3)  {
+                
+                
+                displayDogRecord(prompt);
+
+            } else if (prompt == 4) {
+
+                System.out.println("Program has ended!");
+            }
+            
+
+        } while (prompt != 4);
+
 
     }
 
@@ -83,9 +130,124 @@ public class DogManagement {
         menuOption = Integer.parseInt(scn.nextLine());
 
         return menuOption;
+
+        
     }
 
-  
+    public static void createupdateDogRecord(int index){
+        int id;
+        String name;
+        int weight;
+        int age;
+
+
+
+        if(index < 0){
+            
+                System.out.println("You have selected to enter a new dog.");
+        
+        } 
+
+        System.out.print("Enter dog ID #: ");
+        id = Integer.parseInt(scn.nextLine());
+
+        System.out.print("Enter dog Name: ");
+        name = scn.nextLine();
+
+        System.out.print("Enter dog weight: ");
+        weight = Integer.parseInt(scn.nextLine());
+
+        System.out.print("Enter dog age: ");
+        age = Integer.parseInt(scn.nextLine());
+
+        if(index < 0){
+            dogsIDList.add(id);
+            dogsNameList.add(name);
+            dogsWeightList.add(weight);
+            dogsAgeList.add(age);
+
+        } else {
+            dogsIDList.set(index, id);
+            dogsNameList.set(index, name);
+            dogsWeightList.set(index, weight);
+            dogsAgeList.set(index, age);
+        }
+
+        int[] arrayID = dogsIDList.stream().mapToInt(i -> i).toArray();
+        dogsID = arrayID;
+
+        String [] arrayName = dogsNameList.toArray(new String[dogsNameList.size()]);
+        dogsName = arrayName;
+
+        int[] arrayWeight = dogsWeightList.stream().mapToInt(i -> i).toArray();
+        dogsWeight = arrayWeight;
+
+        int[] arrayAge = dogsAgeList.stream().mapToInt(i -> i).toArray();
+        dogsAge = arrayAge;
+
+        if (index < 0){
+
+            System.out.println("");
+            System.out.println("The following information has been entered:");
+            
+        } else {
+
+            System.out.println("The following information has been updated:");
+
+        } 
+        displayDogInfo(id, name, weight, age);
+
+
+    }
+
+    public static void displayDogInfo(int id, String name, int weight, int age){
+
+        
+        System.out.println("\tID #: " + id);
+        System.out.println("\tName: " + name);
+        System.out.println("\tWeight: " + weight);
+        System.out.println("\tAge: " + age);
+
+
+    }
+
+    public static void displayDogRecord(int choice){
+
+        for(int row = 0; row < dogsID.length; row++){
+
+            System.out.println("ID #: " + dogsID[row] + " for " + dogsName[row]);
+
+        }
+
+        if(choice == 2) {
+            System.out.print("Please enter ID # to from above to display record: ");
+        } else {
+            System.out.print("Please enter the dog ID # to update record: ");
+        }
+            int idRecord = Integer.parseInt(scn.nextLine());
+
+        if(dogsIDList.contains(idRecord) && choice == 2){
+            int index = dogsIDList.indexOf(idRecord);
+            displayDogInfo(dogsID[index], dogsName[index], dogsWeight[index], dogsAge[index]);
+        
+        } else if (dogsIDList.contains(idRecord) && choice == 3) {
+            int index = dogsIDList.indexOf(idRecord);
+            System.out.println("You have selected to update " + dogsName[index]);
+            createupdateDogRecord(index);
+        } else {
+            System.out.println("Id # does not match dog id in system");
+            
+        }
+
+
+    }
+
     
 
+
+
+
+    
 }
+
+
